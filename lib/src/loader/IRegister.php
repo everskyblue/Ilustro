144632387edad9d1eac0dc225382b0a9c3b52ca0
nike madrid <nikejoelmadrid@gmail.com>
2018-09-16 00:25:29 -0500

    registar namespace

---
+++ lib/src/loader/IRegister.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @description registar namespace
+ * @package IRegister
+ */
+class IRegister {
+
+    /**
+     * @var array
+     */
+    protected $namespace = [];
+    
+    /**
+     * @description añade namespace de las clase a incluir
+     * @param string $dir directorio base
+     * @param array $ns registro de namespace
+     */
+    public function add($dir, array $ns)
+    {
+        $this->base_dir = $dir;
+        $this->namespace = array_merge($this->namespace, $ns);
+    }
+    
+    /**
+     * @description se pasa como parametro el nombre
+     * de la clase que se esta llamando y se reemplaza
+     * por la ruta del archivo
+     *
+     * @param string $name nombre de la clase
+     * @return string
+     */
+    public function __get($name)
+    {
+        $rp = ['/', '\\'];
+        foreach($this->namespace as $ns => $dir) {
+            if (strpos($name, $ns)!==false){
+                $path = str_replace($ns, $dir, $name);
+                return $this->absPath($rp, $path);
+            } 
+        }
+        return $this->absPath($rp, $name);
+    }
+    
+    /**
+     * @param array|string $m
+     * @param string $rp
+     * @return string
+     */
+    private function absPath($m, $rp)
+    {
+        return $this->base_dir . str_replace($m, DIRECTORY_SEPARATOR, $rp);
+    }
+}
