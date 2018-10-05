bf9293838cc80718bea198d4b560e89a3a7c20da
nike madrid <nikejoelmadrid@gmail.com>
2018-10-05 02:56:07 -0500

    componentes de la url

---
+++ lib/src/Http/Url/Builder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kowo\Ilustro\Http\Url;
+
+
+class Builder {
+    
+    /**
+     * añadir parametros a la url
+     *
+     * @param string $url
+     * @param array $builder
+     * @return string
+     */
+    public function appendParamsUrlBase(array $builder = [])
+    {
+        $base = $this->url;
+        if(!$builder){
+            return $base;
+        }
+        if(strpos($base, '?') === false){
+          return $base . '?' . http_build_query($builder, null ,'&');
+        }
+        list($path, $query) = explode('?', $base, 2);
+        $extP = [];
+        parse_str($query, $extP);
+        //une el query
+        $builder = array_merge($builder, $extP);
+        //ordena el query
+        ksort($builder);
+        return $path . '?' . http_build_query($builder, null , '&');
+    }
+}
